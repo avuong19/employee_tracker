8d75e6f988b77822d55fa04433363c1f6e24ec6c
Anh Vuong <vta19797@gmail.com>
2022-05-11 00:34:33 -0500

    add show functions for the tables

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const inquirer = require('inquirer');
-const db = require('./db/connection');
+const mysql = require('mysql2');
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'work'
+}, console.log('Connected to the work database.'));
+
+//require('dotenv').config();
 const cTable = require('console.table');
+
+db.connect(function(){
+    console.log('Get Your Employees Information Here');
+    promptUser()
+});
+
 const promptUser = () => {
     inquirer.prompt([{
             name: 'choices',
@@ -8,31 +25,89 @@ const promptUser = () => {
             message: 'Select an option',
             choices: [
                 'View Employees',
-                'View Department ',
+                'View Department',
                 'View all Role',
                 'add a department',
                 'add a role',
                 'add an employee',
-                'update an employee role'
+                'update an employee role',
+                'Close'
             ]
 
         }]).then((answers) => {
-        const {selection} = answers;
-        if (selection === 'View Employees') {
+        
+        if (answers.choices === 'View Employees') {
             viewEmployee();
+        }
+        if (answers.choices === 'Close'){
+            db.end();
+        }
+        if (answers.choices === 'View Department') {
+            viewDepartment();
+        }
+        if (answers.choices === 'View all Role') {
+            viewRole();
+        }
+        if (answers.choices === 'add a department') {
+            addDept();
         }
 
     })
 }
 
 const viewEmployee =() =>{
-    const sql = 'select employee.first_name, employee.last_name, role.title, role.salary, department.dept_name as 'department' from employee, role, department where  department.id=role.dept_id and role.id = employee.role_id;'
+    const sql = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.dept_name, employee.mangager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.id = department.id;'
 
-    console.table (sql);
+    db.query(sql,(err,res)=>{
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    })
+}
+const viewDepartment =() =>{
+    const sql = 'SELECT department.id, department.dept_name FROM department;'
+
+    db.query(sql,(err,res)=>{
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    })
+}
+const viewRole =() =>{
+    const sql = 'SELECT role.id, role.title, role.salary, department.dept_name FROM role INNER JOIN department ON role.dept_id=department.id;'
+
+    db.query(sql,(err,res)=>{
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    })
+}
+const addDept =()=>{
+    inquirer.prompt([{
+        name: 'addDept',
+        type: 'input',
+        message: 'Please type in a new department (Require)',
+        validate: addDeptInput => {
+            if (addDeptInput) {
+              return true;
+            } else {
+              console.log('Please enter a new department !');
+              return false;
+            }
+          }
+
+    }])
+    .then (res =>{
+        const sql ='insert into department (dept_name) values (?);';
+        db.query(sql,res.addDept,(err,res)=>{
+            if (err) throw err;
+            console.table(res);
+            promptUser();
+        })
+    })
 }
 
 
-promptUser();
 
 
 
---
+++ test.js
@@ -0,0 +1,74 @@
+// call once somewhere in the beginning of the app
+const cTable = require('console.table');
+console.table(                 
+    [
+      {
+        first_name: 'Ronald',
+        last_name: 'Firbank',
+        title: 'Civil Engineer',
+        salary: '124000.00',
+        department: 'Engineering'
+      },
+      {
+        first_name: 'Virginia',
+        last_name: 'Woolf',
+        title: 'Lawyer',
+        salary: '125000.00',
+        department: 'Legal'
+      },
+      {
+        first_name: 'Piers',
+        last_name: 'Gaveston',
+        title: 'Accountant',
+        salary: '90000.00',
+        department: 'Finance'
+      },
+      {
+        first_name: 'Charles',
+        last_name: 'LeRoi',
+        title: 'Civil Engineer',
+        salary: '124000.00',
+        department: 'Engineering'
+      },
+      {
+        first_name: 'Katherine',
+        last_name: 'Mansfield',
+        title: 'Lawyer',
+        salary: '125000.00',
+        department: 'Legal'
+      },
+      {
+        first_name: 'Dora',
+        last_name: 'Carrington',
+        title: 'Accountant',
+        salary: '90000.00',
+        department: 'Finance'
+      },
+      {
+        first_name: 'Edward',
+        last_name: 'Bellamy',
+        title: 'Accountant',
+        salary: '90000.00',
+        department: 'Finance'
+      },
+      {
+        first_name: 'Montague',
+        last_name: 'Summers',
+        title: 'Lawyer',
+        salary: '125000.00',
+        department: 'Legal'
+      },
+      {
+        first_name: 'Octavia',
+        last_name: 'Butler',
+        title: 'Civil Engineer',
+        salary: '124000.00',
+        department: 'Engineering'
+      },
+      {
+        first_name: 'Unica',
+        last_name: 'Zurn',
+        title: 'Lawyer',
+        salary: '125000.00',
+        department: 'Legal'
+      }]);
